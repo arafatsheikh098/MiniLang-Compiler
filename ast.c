86ca4d3106d26a329184775856dcb629a9e696a4
Sayeed Mahdi <151737918+sayeed-mahdii@users.noreply.github.com>
2026-07-19 22:26:01 +0600

    Refactor AST node creation functions

--- ast.c
+++ ast.c
@@ -1,99 +1,56 @@
-#include <stdlib.h>
-#include <string.h>
 #include "ast.h"
 
-ASTNode* create_node(NodeType type, int line) {
-    ASTNode *node = (ASTNode*)malloc(sizeof(ASTNode));
-    memset(node, 0, sizeof(ASTNode));
-    node->type = type;
-    node->line_num = line;
-    node->dataType = TYPE_VOID;
-    return node;
-}
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-ASTNode* create_int_literal(int val, int line) {
-    ASTNode *n = create_node(NODE_INT_LITERAL, line);
-    n->int_val = val;
-    n->dataType = TYPE_INT;
+static ASTNode *alloc_node(NodeKind kind, int lineno)
+{
+    ASTNode *n = calloc(1, sizeof(ASTNode));
+    if (!n) { fprintf(stderr, "ast: out of memory\n"); exit(1); }
+    n->kind   = kind;
+    n->dtype  = TYPE_UNKNOWN;
+    n->lineno = lineno;
     return n;
 }
 
-ASTNode* create_bool_literal(int val, int line) {
-    ASTNode *n = create_node(NODE_BOOL_LITERAL, line);
-    n->bool_val = val;
-    n->dataType = TYPE_BOOL;
+ASTNode *ast_int_lit(int val, int lineno)
+{
+    ASTNode *n  = alloc_node(NODE_INT_LIT, lineno);
+    n->u.ival   = val;
+    n->dtype    = TYPE_INT;
     return n;
 }
 
-ASTNode* create_identifier(char *name, int line) {
-    ASTNode *n = create_node(NODE_IDENTIFIER, line);
-    n->name = strdup(name);
+ASTNode *ast_bool_lit(int val, int lineno)
+{
+    ASTNode *n  = alloc_node(NODE_BOOL_LIT, lineno);
+    n->u.bval   = val;
+    n->dtype    = TYPE_BOOL;
     return n;
 }
 
-ASTNode* create_binary_op(Operator op, ASTNode *left, ASTNode *right, int line) {
-    ASTNode *n = create_node(NODE_BINARY_OP, line);
-    n->op = op;
-    n->left = left;
-    n->right = right;
+ASTNode *ast_ident(const char *name, int lineno)
+{
+    ASTNode *n        = alloc_node(NODE_IDENT, lineno);
+    n->u.ident.name   = strdup(name);
     return n;
 }
 
-ASTNode* create_assign(char *name, ASTNode *expr, int line) {
-    ASTNode *n = create_node(NODE_ASSIGN, line);
-    n->name = strdup(name);
-    n->right = expr;
+ASTNode *ast_decl(const char *name, DataType dt, int lineno)
+{
+    ASTNode *n            = alloc_node(NODE_DECL, lineno);
+    n->u.ident.name       = strdup(name);
+    n->u.ident.decl_type  = dt;
+    n->u.ident.init_expr  = NULL;
     return n;
 }
 
-ASTNode* create_var_decl(DataType type, char *name, int line) {
-    ASTNode *n = create_node(NODE_VAR_DECL, line);
-    n->dataType = type;
-    n->name = strdup(name);
+ASTNode *ast_decl_assign(const char *name, DataType dt, ASTNode *init, int lineno)
+{
+    ASTNode *n            = alloc_node(NODE_DECL_ASSIGN, lineno);
+    n->u.ident.name       = strdup(name);
+    n->u.ident.decl_type  = dt;
+    n->u.ident.init_expr  = init;
     return n;
-}
-
-ASTNode* create_if(ASTNode *cond, ASTNode *body, ASTNode *else_body, int line) {
-    ASTNode *n = create_node(NODE_IF, line);
-    n->cond = cond;
-    n->body = body;
-    n->else_body = else_body;
-    return n;
-}
-
-ASTNode* create_while(ASTNode *cond, ASTNode *body, int line) {
-    ASTNode *n = create_node(NODE_WHILE, line);
-    n->cond = cond;
-    n->body = body;
-    return n;
-}
-
-ASTNode* create_print(ASTNode *expr, int line) {
-    ASTNode *n = create_node(NODE_PRINT, line);
-    n->left = expr;
-    return n;
-}
-
-ASTNode* create_block(ASTNode *stmt_list, int line) {
-    ASTNode *n = create_node(NODE_BLOCK, line);
-    n->body = stmt_list;
-    return n;
-}
-
-ASTNode* create_program(ASTNode *stmt_list, int line) {
-    ASTNode *n = create_node(NODE_PROGRAM, line);
-    n->body = stmt_list;
-    return n;
-}
-
-void append_stmt(ASTNode **list, ASTNode *stmt) {
-    if (*list == NULL) {
-        *list = stmt;
-    } else {
-        ASTNode *cur = *list;
-        while (cur->next != NULL) {
-            cur = cur->next;
-        }
-        cur->next = stmt;
-    }
 }
